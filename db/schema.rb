a1e19596ee14713cf6e41ef7be8238a8eae54ffb
thamizh <thamizh@devbrother.com>
2013-07-15 21:15:22 +0530

    Search correction

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130713155309) do
+ActiveRecord::Schema.define(:version => 20130715142200) do
 
   create_table "call_lists", :force => true do |t|
     t.string   "name"
@@ -124,11 +124,12 @@ ActiveRecord::Schema.define(:version => 20130713155309) do
   end
 
   create_table "shortlists", :force => true do |t|
-    t.string   "status"
+    t.string   "status",       :default => "newly_added"
     t.integer  "candidate_id"
     t.integer  "call_list_id"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.datetime "created_at",                              :null => false
+    t.datetime "updated_at",                              :null => false
+    t.date     "left_on"
   end
 
   add_index "shortlists", ["call_list_id"], :name => "index_shortlists_on_call_list_id"
