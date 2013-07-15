08c173847a8988ad709a5f75144474b4220755fb
Senthil <psenthil202@gmail.com>
2013-07-15 20:46:07 +0530

    Cap scrip added

---
+++ config/deploy.rb
@@ -0,0 +1,12 @@
+set :application, 'HootQuest'
+set :scm, :git
+set :repository, "git@github.com:pSenthil202/HootQuest.git"
+set :user, "root"
+set :scm_passphrase, ""
+set :branch, "all_code"
+set :deploy_via, :remote_cache
+require 'capistrano/ext/multistage'
+set :stages, ["staging", "development", "production"]
+set :default_stage, "development"
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
