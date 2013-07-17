fe927207f8cdbd8814042c4fbc9b6900f00dcae1
codingmart <prabhu@codingmart-prabhu.(none)>
2013-07-17 17:49:35 +0530

    designs and validations

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,9 @@
 class HomeController < ApplicationController
-  
+  before_filter :check_login
   def index
   	render layout: false
+  end
+  def check_login
+  	redirect_to search_path if current_user
   end
 end
